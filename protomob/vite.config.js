70045d96096032eaead05a65212c936fff522142
Jarbas Martins <jarbasmartins0708@gmail.com>
2025-07-08 14:25:09 -0300

    rotas: corrigindo rotas 5

--- protomob/vite.config.js
+++ protomob/vite.config.js
@@ -7,14 +7,13 @@ export default defineConfig({
     build: {
         outDir: "dist",
         assetsDir: "assets",
-        sourcemap: true,
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
                 onboarding: resolve(__dirname, "src/pages/onboarding.html"),
                 auth: resolve(__dirname, "src/pages/auth.html"),
-                riskTest: resolve(__dirname, "src/pages/risktest.html"),
                 contact: resolve(__dirname, "src/pages/contact.html"),
+                risktest: resolve(__dirname, "src/pages/risktest.html"),
                 information: resolve(__dirname, "src/pages/information.html"),
             },
         },
